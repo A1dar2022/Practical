4ab9ffc36a1b993c52e71c5e81bd432864220a42
iwerrao <iwerrao@gmail.com>
2022-09-15 23:19:24 +0300

    Всё исправил!

--- Java/src/Task1/Main.java
+++ Java/src/Task1/Main.java
@@ -9,14 +9,13 @@ public class Main {
         Box box = new Box();
         box.create();
 
-        box.getInfoSweetness();
-        System.out.println("");
-
         /*не стал использовать скане и прочий ввод для экономии времени...
         очень мало сна  =)
          */
+        
         box.deleteMinWeight(670);  //При проверке только по весу
         // box.deleteMinPrice(500, 600);  //При проверке  по цене и весу
+
         box.getInfoSweetness();
         box.printPriceBox();
         box.printWeightBox();
